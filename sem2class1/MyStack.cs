16b4f47aeea463c6230b09a2053d02cf9e2cd67a
irekkkarimov <irekkkarimov@mail.ru>
2023-02-09 11:31:52 +0300

    MyStack()

---
+++ sem2class1/MyStack.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+
+namespace sem2class1;
+
+public class MyStack
+{
+    private int[] _stack;
+    private int _counter = -1;
+    
+    public MyStack(int maxSize)
+    {
+        _stack = new int[maxSize];
+    }
+    
+    public void Push(int number)
+    {
+        if (_counter == (_stack.Count() - 1)) throw new ArgumentException("The stack is full");
+        _stack[_counter + 1] = number;
+        _counter++;
+    }
+
+    public int Pop()
+    {
+        if (_counter == -1) throw new ArgumentException("The stack is empty");
+        var res = _stack[_counter];
+        _counter--;
+        return res;
+    }
+
+    public bool IsEmpty()
+    {
+        if (_counter == 0)
+            return true;
+        return false;
+    }
+
+    public void Clear()
+    {
+        _counter = -1;
+        for (int i = 0; i < _stack.Length; i++)
+        {
+            _stack[i] = 0;
+        }
+    }
+
+    public int Count()
+    {
+        return _counter + 1;
+    }
+}
---
+++ sem2class1/Program.cs
@@ -0,0 +1,45 @@
+﻿using sem2class1;
+
+public class Program
+{
+    public static void Main()
+    {
+        int maxSize = int.Parse(Console.ReadLine());
+        var test = new MyStack(10);
+        
+        // stack.IsEmpty() Test
+        Console.WriteLine(test.IsEmpty());
+        
+        // stack.push() Test
+        Random r = new Random();
+        for (int i = 0; i < maxSize; i++)
+        {
+            test.Push(r.Next(-100, 101));
+            Console.WriteLine($"top - {test.Count() - 1}");
+        }
+
+        // stack.Pop() test
+        var temp = test.Count();
+        for (int i = 0; i < temp; i++)
+        {
+            test.Pop();
+            Console.WriteLine($"top - {test.Count() - 1}");
+        }
+        Console.WriteLine("stack length");
+        Console.WriteLine(test.Count());
+        
+        // stack.Clear() test
+        test.Push(10);
+        test.Push(20);
+        
+        Console.WriteLine("stack length");
+        Console.WriteLine(test.Count());
+        test.Clear();
+        Console.WriteLine("stack length");
+        Console.WriteLine(test.Count());
+        Console.WriteLine("arg exception \\/");
+        test.Pop();
+
+
+    }
+}
